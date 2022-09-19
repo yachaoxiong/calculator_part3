2b3418bd5366b7c51548a8529595418e6f0a6e24
DEVOPS.YXiong@crucialinsights.net <DEVOPS.YXiong@crucialinsights.net>
2022-09-19 19:22:02 -0400

    Initial Commit

---
+++ Group9_MAPD14_Assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Group9_MAPD14_Assignment1
+//
+//  Created by Yachao on 2022-09-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
