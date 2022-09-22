9806de18d438bca4dc77f3c1e38bec301ef3b9f6
DEVOPS.YXiong@crucialinsights.net <DEVOPS.YXiong@crucialinsights.net>
2022-09-21 21:04:41 -0400

    added app and team info

--- Group9_MAPD14_Assignment1/ViewController.swift
+++ Group9_MAPD14_Assignment1/ViewController.swift
@@ -1,20 +1,38 @@
 //
 //  ViewController.swift
-//  Group9_MAPD14_Assignment1
+//
+//  APP Name: Calculator APP
+//  Group9_MAPD714_Assignment1
+//  Group Members
+//     - Yachao Xiong  301298033
+//     - Mingyuan Xie  301275467
 //
 //  Created by Yachao on 2022-09-19.
+
+//  Revision History
+//  V1.0 create a basic app                                              - 2022-09-19
+//  V1.1 added background color                                          - 2022-09-19
+//  V1.2 added numbers and decimal point buttons                         - 2022-09-19
+//  V1.3 added operator,backspace,and plus/minus buttons with contraints - 2022-09-20
+//  V1.4 added result label and constraints                              - 2022-09-20
+//  V1.5 added result label and constraints                              - 2022-09-20
+//  V1.6 Added app and group members information                         - 2022-09-21
+//  Last modified Date - 2022-09-21
 //
+//  About The APP
+//  This app is to build a calculation tool that can perform arithmetic operations on numbers.
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var result: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.insetsLayoutMarginsFromSafeArea = false
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
 }
 
