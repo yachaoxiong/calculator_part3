a6312f1ed8a24e99ecf246a38a00e4860b370afd
DEVOPS.YXiong@crucialinsights.net <DEVOPS.YXiong@crucialinsights.net>
2022-10-08 11:35:25 -0400

    added operator fucntion and clear function

--- Group9_MAPD714_Assignment2/ViewController.swift
+++ Group9_MAPD714_Assignment2/ViewController.swift
@@ -38,6 +38,7 @@ class ViewController: UIViewController {
         view.insetsLayoutMarginsFromSafeArea = false
         // Do any additional setup after loading the view.
     }
+    
     @IBAction func digitButton_Pressed(_ sender: UIButton) {
            if hasClickedOperatorButton && hasFinishedInput {
                outputResult_Label.text = ""
@@ -53,7 +54,41 @@ class ViewController: UIViewController {
            hasFinishedInput = false
            hasClickedOperatorButton = false
        }
- 
+    
+    @IBAction func operatorButton_Pressed(_ sender: UIButton) {
+        if sender.titleLabel!.text! == "=" && digitStack.count != 0 {
+            equalButton_Pressed()
+            return
+        }
+        currentResult = Double(outputResult_Label.text!) ?? 0.0
+        if hasClickedOperatorButton == true && !handleChangeOperator(sender: sender){
+            operatorStack.removeLast()
+            operatorStack.append(sender.titleLabel!.text!)
+            hasFinishedInput = true
+            return
+        }
+        hasClickedOperatorButton = true
+        previousOperatorButton = sender.titleLabel!.text!
+        let currentOperator:String = sender.titleLabel!.text!
+        if digitStack.count == 0 {
+            digitStack.append(currentResult)
+            operatorStack.append(currentOperator)
+        }else{
+            digitStack.append(currentResult)
+            //check if previous operator is * or /
+            let lastOperator:String = operatorStack[operatorStack.count-1]
+            if lastOperator == "×" || lastOperator == "÷" {
+                hendleWhenLastOperatorIsMultiplicationOrDivide(lastOperator: lastOperator)
+            }
+            if currentOperator == "×" || currentOperator == "÷" {
+                operatorStack.append(currentOperator)
+            }else{
+                handleWhenCurrentOperatorIsPlusOrMinus(currentOperator:currentOperator)
+            }
+        }
+        hasFinishedInput = true
+    }
+    
     @IBAction func deleteButton_Pressed(_ sender: UIButton) {
           if hasClickedOperatorButton == true { return }
           let text = outputResult_Label.text!
@@ -82,6 +117,109 @@ class ViewController: UIViewController {
         }else if sender.titleLabel!.text! == "%" {
             outputResult_Label.text = String(value/100)
         }
+    }
+    @IBAction func clearButton_Pressed(_ sender: UIButton) {
+           outputResult_Label.text = "0"
+           digitStack.removeAll()
+           operatorStack.removeAll()
+           removedDigitStack.removeAll()
+           removedOperatorStack.removeAll()
+           tempResult = 0
+           lastOperator = ""
+           previousOperatorButton = ""
+           currentResult = 0
+           hasClickedOperatorButton = false
+           hasFinishedInput = false
+       }
+    
+    func hendleWhenLastOperatorIsMultiplicationOrDivide(lastOperator:String){
+         let value1:Double = digitStack.removeLast()
+         let value2:Double = digitStack.removeLast()
+         operatorStack.removeLast()
+         if lastOperator == "×" {
+             tempResult = value1 * value2
+         }else{
+             tempResult = value2 / value1
+         }
+         digitStack.append(tempResult)
+         outputResult_Label.text = handleOutputResult(output:String(tempResult))
+     }
+     
+     func handleWhenCurrentOperatorIsPlusOrMinus(currentOperator:String){
+         if digitStack.count < 2 {
+             operatorStack.append(currentOperator)
+         }else {
+             lastOperator = operatorStack[operatorStack.count-1]
+             let value1:Double = digitStack.removeLast()
+             removedDigitStack.append(value1)
+             let value2:Double = digitStack.removeLast()
+             removedDigitStack.append(value2)
+             
+             if(lastOperator == "+"){
+                 tempResult = value1 + value2
+             }else{
+                 tempResult = value2 - value1
+             }
+             let removedOperator = operatorStack.removeLast()
+             removedOperatorStack.append(removedOperator)
+             
+             digitStack.append(tempResult)
+             operatorStack.append(currentOperator)
+             outputResult_Label.text = handleOutputResult(output:String(tempResult))
+         }
+     }
+    func equalButton_Pressed(){
+          if  digitStack.count == operatorStack.count {
+              if hasFinishedInput == false{
+                  currentResult = Double(outputResult_Label.text!) ?? 0.0
+                  digitStack.append(currentResult)
+              }else{
+                  digitStack.append(digitStack[digitStack.count-1])
+              }
+          }
+          let lastOperator = operatorStack[operatorStack.count-1]
+          while operatorStack.count != 0 {
+              let value1:Double = digitStack.removeLast()
+              let value2:Double = digitStack.removeLast()
+              let currentOperator = operatorStack.removeLast()
+              if currentOperator == "×"{
+                  digitStack.append(value1 * value2)
+              }else if currentOperator == "÷"{
+                  digitStack.append(value2 / value1)
+              }else if currentOperator == "+"{
+                  digitStack.append(value1 + value2)
+              }else if currentOperator == "-"{
+                  digitStack.append(value2 - value1)
+              }
+          }
+          outputResult_Label.text = handleOutputResult(output:String(digitStack[digitStack.count-1]))
+          operatorStack.append(lastOperator)
+          if hasFinishedInput == false {
+              hasFinishedInput = true
+          }
+      }
+    
+    func handleChangeOperator(sender:UIButton)->Bool{
+        if (previousOperatorButton == "+" || previousOperatorButton == "−")
+            && (sender.titleLabel!.text! == "×" || sender.titleLabel!.text! == "÷"){
+            if removedDigitStack.count >= 2 {
+                let origialFirstValue = removedDigitStack.removeLast()
+                let origialSecondValue = removedDigitStack.removeLast()
+                digitStack.removeLast()
+                digitStack.append(origialFirstValue)
+                operatorStack.removeLast()
+                operatorStack.append(removedOperatorStack.removeLast())
+                currentResult = origialSecondValue
+                outputResult_Label.text = handleOutputResult(output:String(currentResult))
+                return true
+            }
+        }else if (previousOperatorButton == "×" || previousOperatorButton == "÷")
+                    && (sender.titleLabel!.text! == "+" || sender.titleLabel!.text! == "-"){
+            currentResult = digitStack.removeLast()
+            operatorStack.removeLast()
+            return true
+        }
+        return false
     }
     
     func handleOutputResult(output:String)->String{
